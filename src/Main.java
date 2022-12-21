dd9c43bc50f9498cc1fadd01ae1d732a6b3aca25
BrankoB123 <kbkzamejl@gamil.com>
2022-12-21 17:32:58 -0500

    the basic project Solar System is ready for review

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Basic structure is ready please write your code in this project");
+        SolarSystem Jinny=new SolarSystem();
+        Jinny.moon=1;
+        Jinny.planets=2;
+        Jinny.sun=3;
+        
+
+
     }
 }
